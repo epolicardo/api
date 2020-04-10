7bba3657ecefc377181cc08040777639db098ce6
Emiliano Policardo <emilianopolicado@gmail.com>
2020-04-09 21:23:01 -0300

    Primero

---
+++ UsersAPI/Data/Modelos/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersAPI.Modelos
+{
+    public class User : IdentityUser
+    {
+        public Persona Persona { get; set; }
+    }
+}
